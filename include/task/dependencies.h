b2446fa23d8e1f1382a66e107046b7d3dbe4e936
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-17 23:41:39 +0800

    bsp::ISysTick &DI_SysTick();

--- include/task/dependencies.h
+++ include/task/dependencies.h
@@ -3,7 +3,8 @@
 
 /// @brief 这里都是本库的依赖项，采用声明函数，让其返回接口实例的引用的方式。这是一种依赖注入模式，
 /// 库的使用者需要实现这些函数，否则将引发链接错误。
+/// @note 这里的函数都以 DI_ 开头。是 dependency injection 的缩写，表示依赖注入。
 namespace task
 {
-	bsp::ISysTick &DEP_SysTick();
+	bsp::ISysTick &DI_SysTick();
 }
