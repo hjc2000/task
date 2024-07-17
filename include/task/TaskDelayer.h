b65e5c94328578f2fb86aa6dd1e5d9b8e2e6dd8d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-17 23:50:19 +0800

    static TaskDelayer &Instance()

--- include/task/TaskDelayer.h
+++ include/task/TaskDelayer.h
@@ -7,6 +7,12 @@ namespace task
 		: public bsp::IDelayer
 	{
 	public:
+		static TaskDelayer &Instance()
+		{
+			static TaskDelayer o;
+			return o;
+		}
+
 		void Delay(std::chrono::microseconds microseconds);
 		void Delay(std::chrono::milliseconds milliseconds);
 		void Delay(std::chrono::seconds seconds);
