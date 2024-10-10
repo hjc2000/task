8795e2a9fe7d4abd8e8c4f50389a2e93f338cc0a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-10-10 22:12:28 +0800

    TaskManager

--- private_src/TaskManager.cpp
+++ private_src/TaskManager.cpp
@@ -1,1 +1,27 @@
 #include "TaskManager.h"
+
+bsp::TaskManager &bsp::TaskManager::Instance()
+{
+    class Getter :
+        public base::SingletonGetter<TaskManager>
+    {
+    public:
+        std::unique_ptr<TaskManager> Create() override
+        {
+            return std::unique_ptr<TaskManager>{new TaskManager{}};
+        }
+
+        void Lock() override
+        {
+            DI_InterruptSwitch().DisableGlobalInterrupt();
+        }
+
+        void Unlock() override
+        {
+            DI_InterruptSwitch().EnableGlobalInterrupt();
+        }
+    };
+
+    Getter g;
+    return g.Instance();
+}
--- private_src/TaskManager.h
+++ private_src/TaskManager.h
@@ -3,7 +3,7 @@
 #include <base/di/SingletonGetter.h>
 #include <bsp-interface/di/interrupt.h>
 #include <bsp-interface/task/ITaskManager.h>
-#include <task/Task.h>
+#include <FreeTask.h>
 
 namespace bsp
 {
@@ -14,31 +14,7 @@ namespace bsp
         TaskManager() = default;
 
     public:
-        static_function TaskManager &Instance()
-        {
-            class Getter :
-                public base::SingletonGetter<TaskManager>
-            {
-            public:
-                std::unique_ptr<TaskManager> Create() override
-                {
-                    return std::unique_ptr<TaskManager>{new TaskManager{}};
-                }
-
-                void Lock() override
-                {
-                    DI_InterruptSwitch().DisableGlobalInterrupt();
-                }
-
-                void Unlock() override
-                {
-                    DI_InterruptSwitch().EnableGlobalInterrupt();
-                }
-            };
-
-            Getter g;
-            return g.Instance();
-        }
+        static_function TaskManager &Instance();
 
         /// @brief 启动运行一个任务
         ///
@@ -48,7 +24,7 @@ namespace bsp
         /// @note 对于 32 位宽的 CPU ，一个字是 32 位，即 4 个字节。
         std::shared_ptr<bsp::ITask> Create(std::function<void()> func, uint32_t stack_size) override
         {
-            return task::Task::Create(func, stack_size);
+            return task::FreeTask::Create(func, stack_size);
         }
     };
 } // namespace bsp
