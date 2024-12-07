edf39cd8469e3b893a35707660c00c3be6013062
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-08 00:22:48 +0800

    使用分数类简化延时的计算

--- private_src/TaskDelayer.cpp
+++ private_src/TaskDelayer.cpp
@@ -1,4 +1,6 @@
 #include "TaskDelayer.h"
+#include <base/unit/Hz.h>
+#include <base/unit/Seconds.h>
 #include <bsp-interface/di/delayer.h>
 #include <bsp-interface/di/systick.h>
 #include <FreeRTOS.h>
@@ -6,7 +8,8 @@
 
 task::TaskDelayer &task::TaskDelayer::Instance()
 {
-    class Getter : public base::SingletonGetter<TaskDelayer>
+    class Getter :
+        public base::SingletonGetter<TaskDelayer>
     {
     public:
         std::unique_ptr<TaskDelayer> Create() override
@@ -33,10 +36,9 @@ void task::TaskDelayer::Delay(std::chrono::microseconds microseconds)
 {
     // 大于 1000 的微秒转换为毫秒，利用 Delay(std::chrono::milliseconds milliseconds) 重载。
     std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(microseconds);
-
     if (ms.count() > 0)
     {
-        Delay(std::chrono::milliseconds{ms});
+        Delay(ms);
     }
 
     // 剩余的小于 1000 部分的微秒
@@ -53,40 +55,21 @@ void task::TaskDelayer::Delay(std::chrono::milliseconds milliseconds)
         return;
     }
 
-    /*
-     * FreeRTOSConfig.h 中的 configTICK_RATE_HZ 定义了 freertos 的 tick 频率。
-     * 注意，这个频率不是 SysTickClock 硬件时钟的频率。
-     *
-     * freertos tick 周期
-     *	T = 1 / configTICK_RATE_HZ
-     * 单位为秒
-     *
-     * 要延时的 tick 数为
-     *	tick = milliseconds / 1000 / T
-     *	tick = milliseconds / 1000 * configTICK_RATE_HZ
-     * 为了防止 milliseconds / 1000 出现截断误差，调整为
-     *	tick = milliseconds * configTICK_RATE_HZ / 1000
-     *
-     * 记
-     *	count = milliseconds * configTICK_RATE_HZ
-     * 求出
-     *	base = count / 1000
-     * 这部分可以用 freertos 延时。
-     *	mod = count % 1000
-     * 这部分利用 freertos 延时的话会被截断，所以用 SysTickClock::Delay 延时。
-     */
+    base::Seconds freertos_tick_interval{base::Hz{configTICK_RATE_HZ}};
+    base::Seconds period{milliseconds};
 
-    int64_t count = milliseconds.count() * configTICK_RATE_HZ;
-    int64_t base = count / 1000;
-    int64_t mod = count % 1000;
-    if (base > 0)
+    // 先让 freertos 把整数个滴答延时完毕
+    base::Fraction tick_count = static_cast<base::Fraction>(period / freertos_tick_interval);
+    if (static_cast<int64_t>(tick_count) > 0)
     {
-        vTaskDelay(base);
+        vTaskDelay(static_cast<int64_t>(tick_count));
     }
 
-    if (mod > 0)
+    // 剩下的需要延时的时间小于 freertos 的滴答周期，无法用 freertos 延时
+    base::Seconds remain = period - freertos_tick_interval * static_cast<int64_t>(tick_count);
+    if (remain > 0)
     {
-        DI_SysTick().Delay(std::chrono::milliseconds{mod / configTICK_RATE_HZ});
+        DI_SysTick().Delay(static_cast<std::chrono::milliseconds>(remain));
     }
 }
 
