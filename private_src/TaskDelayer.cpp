4ffd17e8380b85d467d0cc8cbfb6c08c4c973c96
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-08 11:17:50 +0800

    int64_t tick_count = static_cast<int64_t>(period / freertos_tick_interval);

--- private_src/TaskDelayer.cpp
+++ private_src/TaskDelayer.cpp
@@ -59,14 +59,14 @@ void task::TaskDelayer::Delay(std::chrono::milliseconds milliseconds)
     base::Seconds period{milliseconds};
 
     // 先让 freertos 把整数个滴答延时完毕
-    base::Fraction tick_count = static_cast<base::Fraction>(period / freertos_tick_interval);
-    if (static_cast<int64_t>(tick_count) > 0)
+    int64_t tick_count = static_cast<int64_t>(period / freertos_tick_interval);
+    if (tick_count > 0)
     {
-        vTaskDelay(static_cast<int64_t>(tick_count));
+        vTaskDelay(tick_count);
     }
 
     // 剩下的需要延时的时间小于 freertos 的滴答周期，无法用 freertos 延时
-    base::Seconds remain = period - freertos_tick_interval * static_cast<int64_t>(tick_count);
+    base::Seconds remain = period - freertos_tick_interval * tick_count;
     if (remain > 0)
     {
         DI_SysTick().Delay(static_cast<std::chrono::milliseconds>(remain));
