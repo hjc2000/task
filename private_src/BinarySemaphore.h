3b20777e452fea6d1111cb3badca3445ff250c87
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-11 23:24:31 +0800

    BinarySemaphore

--- private_src/BinarySemaphore.h
+++ private_src/BinarySemaphore.h
@@ -18,9 +18,25 @@ namespace task
         BinarySemaphore();
         ~BinarySemaphore();
 
+        /// @brief 释放信号量。
         void Release() override;
+
+        /// @brief 在中断中释放信号量。
         void ReleaseFromISR() override;
+
+        /// @brief 获取信号量。无限等待，永不超时。
         void Acquire() override;
+
+        /// @brief 尝试获取信号量。超时后会失败，并返回 false，成功获取信号量后将
+        /// 返回 true.
+        /// @param timeout 超时时间。
+        /// @return
+        bool TryAcquire(base::Seconds const &timeout) override;
+
+        /// @brief 尝试获取信号量。超时后会失败，并返回 false，成功获取信号量后将
+        /// 返回 true.
+        /// @param ticks 超时时间，单位：freertos 的 tick 数。
+        /// @return
         bool TryAcquire(TickType_t ticks = 0);
     };
 } // namespace task
