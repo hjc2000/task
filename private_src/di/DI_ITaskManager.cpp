d888dcff76b630f8edc9fdec0c50af15c87d68c8
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-11 16:39:26 +0800

    DI_CreateTask

--- private_src/di/DI_ITaskManager.cpp
+++ private_src/di/DI_ITaskManager.cpp
@@ -1,9 +1,38 @@
+#include <base/define.h>
+#include <base/di/SingletonGetter.h>
+#include <bsp-interface/di/interrupt.h>
+#include <bsp-interface/di/systick.h>
 #include <bsp-interface/di/task.h>
-#include <TaskManager.h>
+#include <FreeTask.h>
 
-bsp::ITaskManager &DI_TaskManager()
+/// @brief 创建一个任务
+///
+/// @param func 任务函数
+///
+/// @param stack_size 任务栈大小。单位：字。
+/// @note 对于 32 位宽的 CPU ，一个字是 32 位，即 4 个字节。
+std::shared_ptr<bsp::ITask> DI_CreateTask(std::function<void()> func, uint32_t stack_size)
 {
-	return bsp::TaskManager::Instance();
+	return task::FreeTask::Create(func, stack_size);
+}
+
+/// @brief 启动调度。本函数会持续阻塞。
+void DI_StartScheduler()
+{
+	/* 启动调度前需要设置回调函数，这个回调函数由 SysTick 定时时间到中断触发，
+	 * 在这里面定期调用 xPortSysTickHandler，它是 freertos 时间的来源，没有它，
+	 * freertos 的与时间相关的功能都不能工作。
+	 */
+	DI_SysTick().SetElapsedHandler(
+		[]()
+		{
+			if (xTaskGetSchedulerState() != taskSCHEDULER_NOT_STARTED)
+			{
+				xPortSysTickHandler();
+			}
+		});
+
+	vTaskStartScheduler();
 }
 
 /// @brief 暂停所有调度。
