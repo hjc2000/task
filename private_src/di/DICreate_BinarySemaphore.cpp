3960ec3a7dd28cfacbcc987c6610c7ebdf9b8574
hjc2000 <hjc.666@foxmail.com>
2025-01-17 11:07:33 +0800

    bsp::di::task::

--- private_src/di/DICreate_BinarySemaphore.cpp
+++
@@ -1,7 +0,0 @@
-#include <BinarySemaphore.h>
-#include <bsp-interface/di/task.h>
-
-std::shared_ptr<bsp::IBinarySemaphore> DICreate_BinarySemaphore()
-{
-	return std::shared_ptr<bsp::IBinarySemaphore>{new task::BinarySemaphore{}};
-}
--- private_src/di/DI_ITaskManager.cpp
+++ private_src/di/DI_ITaskManager.cpp
@@ -1,20 +1,26 @@
 #include <base/define.h>
+#include <BinarySemaphore.h>
 #include <bsp-interface/di/interrupt.h>
 #include <bsp-interface/di/systick.h>
 #include <bsp-interface/di/task.h>
 #include <FreeTask.h>
 
+std::shared_ptr<bsp::IBinarySemaphore> bsp::di::task::CreateBinarySemaphore()
+{
+	return std::shared_ptr<bsp::IBinarySemaphore>{new ::task::BinarySemaphore{}};
+}
+
 /// @brief 创建一个任务
 /// @param stack_size 任务栈大小。单位：字。
 /// @note 对于 32 位宽的 CPU ，一个字是 32 位，即 4 个字节。
 /// @param func 任务函数
-std::shared_ptr<bsp::ITask> DI_CreateTask(uint32_t stack_size, std::function<void()> func)
+std::shared_ptr<bsp::ITask> bsp::di::task::CreateTask(uint32_t stack_size, std::function<void()> func)
 {
-	return task::FreeTask::Create(func, stack_size);
+	return ::task::FreeTask::Create(func, stack_size);
 }
 
 /// @brief 启动调度。本函数会持续阻塞。
-void DI_StartScheduler()
+void bsp::di::task::StartScheduler()
 {
 	/* 启动调度前需要设置回调函数，这个回调函数由 SysTick 定时时间到中断触发，
 	 * 在这里面定期调用 xPortSysTickHandler，它是 freertos 时间的来源，没有它，
@@ -34,7 +40,7 @@ void DI_StartScheduler()
 
 /// @brief 暂停所有调度。
 /// @warning 实现本函数时禁止使用动态内存分配，因为堆也需要本函数。
-void DI_SuspendAllTask()
+void bsp::di::task::SuspendAllTask()
 {
 	vTaskSuspendAll();
 }
@@ -42,7 +48,7 @@ void DI_SuspendAllTask()
 /// @brief 恢复所有调度。
 /// @note 如果调度器没有启动，本函数不会启动调度器。
 /// @warning 实现本函数时禁止使用动态内存分配，因为堆也需要本函数。
-void DI_ResumeAllTask()
+void bsp::di::task::ResumeAllTask()
 {
 	xTaskResumeAll();
 }
