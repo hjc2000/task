ffd791eb923333eb7d11efc32c58857defcbab3d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-14 00:25:27 +0800

    target_install_obj_dir

--- project.cmake
+++ project.cmake
@@ -1,6 +1,5 @@
 add_library(${ProjectName} OBJECT)
 target_import_src(${ProjectName})
-target_install_obj_dir(${ProjectName})
 
 target_import_freertos(${ProjectName} PUBLIC)
 target_import_base(${ProjectName} PUBLIC)
