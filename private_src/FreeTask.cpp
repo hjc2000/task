52a31cedd66880759bf4a30f23fb0a01ec45f415
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-22 00:42:20 +0800

    base::console

--- private_src/FreeTask.cpp
+++ private_src/FreeTask.cpp
@@ -1,5 +1,5 @@
 #include "FreeTask.h"
-#include <bsp-interface/di/console.h>
+#include "base/Console.h"
 #include <stdexcept>
 
 std::shared_ptr<task::FreeTask> task::FreeTask::Create(std::function<void()> func, uint16_t stack_depth)
@@ -30,12 +30,12 @@ std::shared_ptr<task::FreeTask> task::FreeTask::Create(std::function<void()> fun
 		}
 		catch (std::exception const &e)
 		{
-			bsp::di::Console().WriteError("任务函数发生了异常：");
-			bsp::di::Console().WriteError(e.what());
+			base::console.WriteErrorLine("任务函数发生了异常：");
+			base::console.WriteErrorLine(e.what());
 		}
 		catch (...)
 		{
-			bsp::di::Console().WriteError("任务函数发生了未知的异常。");
+			base::console.WriteErrorLine("任务函数发生了未知的异常。");
 		}
 
 		// 任务结束后需要将 task 从 map 中移除，保证指向的 task::Task 对象能够析构。
@@ -60,6 +60,6 @@ extern "C"
 {
 	void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
 	{
-		bsp::di::Console().WriteError(std::string{"任务栈发生溢出。，任务名："} + pcTaskName);
+		base::console.WriteErrorLine(std::string{"任务栈发生溢出。，任务名："} + pcTaskName);
 	}
 }
